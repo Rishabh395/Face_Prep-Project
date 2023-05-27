c2ba75c019ac6c381a134ef245d9f35272133e9a
Rishabh <rishabhpratap9719@h=gmail.com>
2023-05-27 12:18:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import LoginPage from './components/LoginPage'
+import { BrowserRouter as Router
+  , Routes
+  , Route } from "react-router-dom"
+import Home from './components/Home';
+import './App.css'
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* <LoginPage/> */}
+    <Router>
+      <Routes>
+        <Route path='/'  element={<LoginPage/>} ></Route>
+        <Route path='/home' element={<Home/>}></Route>
+      </Routes>
+    </Router>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/LoginPage.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import '../App.css';
+
+const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(username==="foo" && password==="bar"){
+      localStorage.setItem('login' , 'yes')
+    
+         navigate('/home')
+      
+    }
+    else 
+    {
+      alert("please enter correct details")
+    }
+  };
+
+  return (
+    <div className="login-page">
+      <div className="login-form">
+        <h2>Login Page</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={handleUsernameChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
